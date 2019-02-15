4032165f406bc64ca7231ba0f3bc5c3230b44ec5
Tomas Slusny <slusnucky@gmail.com>
2019-02-15 01:04:04 +0100

    Add more desktop compatibility
    https://github.com/AppImage/AppImageKit/wiki/AppDir
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- appimage/prepare-appimage.sh
+++ appimage/prepare-appimage.sh
@@ -2,3 +2,7 @@
 
 cd $1
 ln -s RuneLite AppRun
+mkdir -p usr/share/applications/
+mkdir -p usr/share/icons/hicolor/512x512/
+cp runelite.png usr/share/icons/hicolor/512x512/runelite.png
+cp runelite.desktop usr/share/applications/runelite.desktop
