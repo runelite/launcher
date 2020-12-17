5a5301342aed412f17776a22ac45146564d26913
Adam <Adam@sigterm.info>
2020-12-17 15:07:10 -0500

    Add linux aarch64 support

---
+++ build-linux-aarch64.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+echo "Building 64 bit executable"
+./fips set config linux-make-release
+./fips clean && ./fips build
+
+echo "Copying 64 bit executable ..."
+cp ../fips-deploy/packr/linux-make-release/packr ../../resources/packr-linux-aarch64
