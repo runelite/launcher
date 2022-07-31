cc15c45a01c2896bc29db9ee600093e8e8cc8332
Adam <Adam@sigterm.info>
2022-07-31 14:22:17 -0400

    macos x64: update to jdk 11.0.16-8

--- build-osx-x64.sh
+++ build-osx-x64.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-JDK_VER="11.0.14.1"
-JDK_BUILD="1"
+JDK_VER="11.0.16"
+JDK_BUILD="8"
 PACKR_VERSION="runelite-1.4"
 
 SIGNING_IDENTITY="Developer ID Application"
@@ -13,7 +13,7 @@ if ! [ -f OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz ] ; then
         https://github.com/adoptium/temurin11-binaries/releases/download/jdk-${JDK_VER}%2B${JDK_BUILD}/OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
 fi
 
-echo "1b2f792ad05af9dba876db962c189527e645b48f50ceb842b4e39169de553303  OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz" | shasum -c
+echo "952fe98f6fe466a83b59ad93357bcf48c36a81596b260026d96f29bc84f0d6c4  OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz" | shasum -c
 
 # packr requires a "jdk" and pulls the jre from it - so we have to place it inside
 # the jdk folder at jre/
