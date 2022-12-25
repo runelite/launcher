debef494a42c3a26bf36e0fefe55c36e5fc675be
Adam <Adam@sigterm.info>
2022-12-24 19:35:58 -0500

    Add macos update functionality

--- src/main/java/net/runelite/launcher/Updater.java
+++ src/main/java/net/runelite/launcher/Updater.java
@@ -35,43 +35,264 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.nio.channels.FileChannel;
 import java.nio.file.AtomicMoveNotSupportedException;
+import java.nio.file.CopyOption;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Instant;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import joptsimple.OptionSet;
 import lombok.extern.slf4j.Slf4j;
+import static net.runelite.launcher.Launcher.LAUNCHER_EXECUTABLE_NAME_OSX;
 import static net.runelite.launcher.Launcher.LAUNCHER_EXECUTABLE_NAME_WIN;
 import static net.runelite.launcher.Launcher.compareVersion;
 import static net.runelite.launcher.Launcher.download;
 import static net.runelite.launcher.Launcher.regQueryString;
 import net.runelite.launcher.beans.Bootstrap;
 import net.runelite.launcher.beans.Update;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 @Slf4j
 class Updater
 {
 	private static final String LAUNCHER_SETTINGS = "settings.json";
+	private static final String RUNELITE_APP = "/Applications/RuneLite.app";
 
 	static void update(Bootstrap bootstrap, OptionSet options, String[] args)
 	{
-		if (OS.getOs() != OS.OSType.Windows)
+		if (OS.getOs() == OS.OSType.Windows)
+		{
+			updateWindows(bootstrap, options, args);
+		}
+		else if (OS.getOs() == OS.OSType.MacOS)
+		{
+			updateMacos(bootstrap, options, args);
+		}
+	}
+
+	private static void updateMacos(Bootstrap bootstrap, OptionSet options, String[] args)
+	{
+		ProcessHandle current = ProcessHandle.current();
+		var command = current.info().command();
+		if (command.isEmpty())
+		{
+			log.debug("Running process has no command");
+			return;
+		}
+
+		Path path = Paths.get(command.get());
+
+		// on macOS packr changes the cwd to the resource directory prior to launching the JVM,
+		// causing current.info().command() to return /Applications/RuneLite.app/Contents/Resources/./RuneLite
+		// despite the executable really being at /Applications/RuneLite.app/Contents/MacOS/RuneLite
+		path = path.normalize()
+			.resolveSibling(Path.of("..", "MacOS", path.getFileName().toString()))
+			.normalize();
+
+		if (!path.getFileName().toString().equals(LAUNCHER_EXECUTABLE_NAME_OSX) || !path.startsWith(RUNELITE_APP))
+		{
+			log.debug("Skipping update check due to not running from installer, command is {}",
+				command.get());
+			return;
+		}
+
+		log.debug("Running from installer");
+
+		var newestUpdate = findAvailableUpdate(bootstrap);
+		if (newestUpdate == null)
 		{
 			return;
 		}
 
+		final boolean noupdate = options.has("noupdate");
+		if (noupdate)
+		{
+			log.info("Skipping update {} due to noupdate being set", newestUpdate.getVersion());
+			return;
+		}
+
+		if (System.getenv("RUNELITE_UPGRADE") != null)
+		{
+			log.info("Skipping update {} due to launching from an upgrade", newestUpdate.getVersion());
+			return;
+		}
+
+		var settings = loadSettings();
+		var hours = 1 << Math.min(9, settings.lastUpdateAttemptNum); // 512 hours = ~21 days
+		if (newestUpdate.getHash().equals(settings.lastUpdateHash)
+			&& Instant.ofEpochMilli(settings.lastUpdateAttemptTime).isAfter(Instant.now().minus(hours, ChronoUnit.HOURS)))
+		{
+			log.info("Previous upgrade attempt to {} was at {} (backoff: {} hours), skipping", newestUpdate.getVersion(),
+				// logback logs are in local time, so use that to match it
+				LocalTime.from(Instant.ofEpochMilli(settings.lastUpdateAttemptTime).atZone(ZoneId.systemDefault())),
+				hours);
+			return;
+		}
+
+		// check if rollout allows this update
+		// there is no installer on macos to write install_id, so just use random()
+		if (newestUpdate.getRollout() > 0. && Math.random() > newestUpdate.getRollout())
+		{
+			log.info("Skipping update {} due to rollout", newestUpdate.getVersion());
+			return;
+		}
+
+		// from here and below the update will be attempted. update settings early so a failed
+		// download counts as an attempt.
+		settings.lastUpdateAttemptTime = System.currentTimeMillis();
+		settings.lastUpdateHash = newestUpdate.getHash();
+		settings.lastUpdateAttemptNum++;
+		saveSettings(settings);
+
+		try
+		{
+			log.info("Downloading launcher {} from {}", newestUpdate.getVersion(), newestUpdate.getUrl());
+
+			var file = Files.createTempFile("rlupdate", "dmg");
+			try (OutputStream fout = Files.newOutputStream(file))
+			{
+				final var name = newestUpdate.getName();
+				final var size = newestUpdate.getSize();
+				try
+				{
+					download(newestUpdate.getUrl(), newestUpdate.getHash(), (completed) ->
+							SplashScreen.stage(.07, 1., null, name, completed, size, true),
+						fout);
+				}
+				catch (VerificationException e)
+				{
+					log.error("unable to verify update", e);
+					file.toFile().delete();
+					return;
+				}
+			}
+
+			log.debug("Mounting dmg {}", file);
+
+			var pb = new ProcessBuilder(
+				"hdiutil",
+				"attach",
+				"-nobrowse",
+				"-plist",
+				file.toAbsolutePath().toString()
+			);
+			Process process = pb.start();
+			if (!process.waitFor(5, TimeUnit.SECONDS))
+			{
+				// timeout
+				process.destroy();
+				log.error("timeout waiting for hdiutil to attach dmg");
+				return;
+			}
+			if (process.exitValue() != 0)
+			{
+				log.error("error running hdiutil attach");
+				return;
+			}
+			String mountPoint;
+			try (var in = process.getInputStream())
+			{
+				mountPoint = parseHdiutilPlist(in);
+			}
+
+			// point of no return
+			log.debug("Removing old install from {}", RUNELITE_APP);
+			delete(Path.of(RUNELITE_APP));
+
+			log.debug("Copying new install from {}", mountPoint);
+			copy(Path.of(mountPoint, "RuneLite.app"), Path.of(RUNELITE_APP));
+
+			log.debug("Unmounting dmg");
+			pb = new ProcessBuilder(
+				"hdiutil",
+				"detach",
+				mountPoint
+			);
+			pb.start();
+
+			log.debug("Done! Launching...");
+
+			List<String> launchCmd = new ArrayList<>(args.length + 1);
+			launchCmd.add(path.toAbsolutePath().toString());
+			launchCmd.addAll(Arrays.asList(args));
+			pb = new ProcessBuilder(launchCmd);
+			pb.environment().put("RUNELITE_UPGRADE", "1");
+			pb.start();
+
+			System.exit(0);
+		}
+		catch (Exception e)
+		{
+			log.error("error performing upgrade", e);
+		}
+	}
+
+	static String parseHdiutilPlist(InputStream in) throws Exception
+	{
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.parse(in);
+		doc.getDocumentElement().normalize();
+
+		Element plist = (Element) doc.getElementsByTagName("plist").item(0);
+		Element dict = (Element) plist.getElementsByTagName("dict").item(0);
+		Element arr = (Element) dict.getElementsByTagName("array").item(0);
+		NodeList dicts = arr.getElementsByTagName("dict");
+		for (int i = 0; i < dicts.getLength(); ++i)
+		{
+			NodeList dict2 = (NodeList) dicts.item(i);
+			String lastKey = null;
+
+			for (int j = 0; j < dict2.getLength(); ++j)
+			{
+				Node node = dict2.item(j);
+
+				if (node.getNodeType() == Node.ELEMENT_NODE)
+				{
+					if (node.getNodeName().equals("key"))
+					{
+						lastKey = node.getTextContent();
+					}
+					else if (lastKey != null)
+					{
+						if (lastKey.equals("mount-point"))
+						{
+							return node.getTextContent();
+						}
+
+						lastKey = null;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	private static void updateWindows(Bootstrap bootstrap, OptionSet options, String[] args)
+	{
 		ProcessHandle current = ProcessHandle.current();
 		if (current.info().command().isEmpty())
 		{
@@ -102,35 +323,7 @@ class Updater
 
 		log.debug("Running from installer");
 
-		var updates = bootstrap.getUpdates();
-		if (updates == null)
-		{
-			return;
-		}
-
-		final var os = System.getProperty("os.name");
-		final var arch = System.getProperty("os.arch");
-		final var launcherVersion = LauncherProperties.getVersion();
-		if (os == null || arch == null || launcherVersion == null)
-		{
-			return;
-		}
-
-		Update newestUpdate = null;
-		for (var update : updates)
-		{
-			var updateOs = OS.parseOs(update.getOs());
-			if ((updateOs == OS.OSType.Other ? update.getOs().equals(os) : updateOs == OS.getOs()) &&
-				(update.getArch() == null || arch.equals(update.getArch())) &&
-				compareVersion(update.getVersion(), launcherVersion) > 0 &&
-				(update.getMinimumVersion() == null || compareVersion(launcherVersion, update.getMinimumVersion()) >= 0) &&
-				(newestUpdate == null || compareVersion(update.getVersion(), newestUpdate.getVersion()) > 0))
-			{
-				log.info("Update {} is available", update.getVersion());
-				newestUpdate = update;
-			}
-		}
-
+		var newestUpdate = findAvailableUpdate(bootstrap);
 		if (newestUpdate == null)
 		{
 			return;
@@ -254,6 +447,40 @@ class Updater
 		}
 	}
 
+	private static Update findAvailableUpdate(Bootstrap bootstrap)
+	{
+		var updates = bootstrap.getUpdates();
+		if (updates == null)
+		{
+			return null;
+		}
+
+		final var os = System.getProperty("os.name");
+		final var arch = System.getProperty("os.arch");
+		final var launcherVersion = LauncherProperties.getVersion();
+		if (os == null || arch == null || launcherVersion == null)
+		{
+			return null;
+		}
+
+		Update newestUpdate = null;
+		for (var update : updates)
+		{
+			var updateOs = OS.parseOs(update.getOs());
+			if ((updateOs == OS.OSType.Other ? update.getOs().equals(os) : updateOs == OS.getOs()) &&
+				(update.getArch() == null || arch.equals(update.getArch())) &&
+				compareVersion(update.getVersion(), launcherVersion) > 0 &&
+				(update.getMinimumVersion() == null || compareVersion(launcherVersion, update.getMinimumVersion()) >= 0) &&
+				(newestUpdate == null || compareVersion(update.getVersion(), newestUpdate.getVersion()) > 0))
+			{
+				log.info("Update {} is available", update.getVersion());
+				newestUpdate = update;
+			}
+		}
+
+		return newestUpdate;
+	}
+
 	@Nonnull
 	private static LauncherSettings loadSettings()
 	{
@@ -330,5 +557,50 @@ class Updater
 			log.warn("unable to get install rollout", ex);
 		}
 		return Math.random();
+	}
+
+	// https://stackoverflow.com/a/27917071
+	private static void delete(Path directory) throws IOException
+	{
+		Files.walkFileTree(directory, new SimpleFileVisitor<>()
+		{
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
+			{
+				Files.delete(file);
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException
+			{
+				Files.delete(dir);
+				return FileVisitResult.CONTINUE;
+			}
+		});
+	}
+
+	// https://stackoverflow.com/a/60621544
+	private static void copy(Path source, Path target, CopyOption... options)
+		throws IOException
+	{
+		Files.walkFileTree(source, new SimpleFileVisitor<>()
+		{
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+				throws IOException
+			{
+				Files.createDirectories(target.resolve(source.relativize(dir).toString()));
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+				throws IOException
+			{
+				Files.copy(file, target.resolve(source.relativize(file).toString()), options);
+				return FileVisitResult.CONTINUE;
+			}
+		});
 	}
 }
