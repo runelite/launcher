560790362ba82f72c51c3548b557358f0fccc196
Adam <Adam@sigterm.info>
2023-06-25 17:16:42 -0400

    build-win-aarch64: update module list
    This is taken from java --list-modules from the temurin jre

--- build-win-aarch64.sh
+++ build-win-aarch64.sh
@@ -25,17 +25,57 @@ if ! [ -d win-aarch64-jdk ] ; then
       --no-man-pages \
       --output win-aarch64-jdk/jre \
       --module-path jdk-$WIN_AARCH64_VERSION/jmods \
-      --add-modules java.base,java.compiler,java.datatransfer,java.xml,java.prefs,java.desktop,java.instrument \
-      --add-modules java.logging,java.management,java.security.sasl,java.naming,java.rmi,java.management.rmi \
-      --add-modules java.net.http,java.scripting,java.security.jgss,java.transaction.xa,java.sql,java.sql.rowset \
-      --add-modules java.xml.crypto,java.se,java.smartcardio,jdk.accessibility,jdk.internal.jvmstat,jdk.attach \
-      --add-modules jdk.charsets,jdk.compiler,jdk.crypto.ec,jdk.crypto.cryptoki,jdk.crypto.mscapi,jdk.dynalink \
-      --add-modules jdk.internal.ed,jdk.editpad,jdk.hotspot.agent,jdk.httpserver,jdk.internal.le,jdk.internal.opt \
-      --add-modules jdk.jartool,jdk.javadoc,jdk.jcmd,jdk.management,jdk.management.agent,jdk.jconsole,jdk.jdeps \
-      --add-modules jdk.jdwp.agent,jdk.jdi,jdk.jfr,jdk.jlink,jdk.jshell,jdk.jsobject,jdk.jstatd,jdk.localedata \
-      --add-modules jdk.management.jfr,jdk.naming.dns,jdk.naming.ldap,jdk.naming.rmi,jdk.net,jdk.pack,jdk.rmic \
-      --add-modules jdk.scripting.nashorn,jdk.scripting.nashorn.shell,jdk.sctp,jdk.security.auth,jdk.security.jgss \
-      --add-modules jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,jdk.zipfs
+      --add-modules java.base \
+      --add-modules java.compiler \
+      --add-modules java.datatransfer \
+      --add-modules java.desktop \
+      --add-modules java.instrument \
+      --add-modules java.logging \
+      --add-modules java.management \
+      --add-modules java.management.rmi \
+      --add-modules java.naming \
+      --add-modules java.net.http \
+      --add-modules java.prefs \
+      --add-modules java.rmi \
+      --add-modules java.scripting \
+      --add-modules java.se \
+      --add-modules java.security.jgss \
+      --add-modules java.security.sasl \
+      --add-modules java.smartcardio \
+      --add-modules java.sql \
+      --add-modules java.sql.rowset \
+      --add-modules java.transaction.xa \
+      --add-modules java.xml \
+      --add-modules java.xml.crypto \
+      --add-modules jdk.accessibility \
+      --add-modules jdk.charsets \
+      --add-modules jdk.crypto.cryptoki \
+      --add-modules jdk.crypto.ec \
+      --add-modules jdk.crypto.mscapi \
+      --add-modules jdk.dynalink \
+      --add-modules jdk.httpserver \
+      --add-modules jdk.internal.ed \
+      --add-modules jdk.internal.le \
+      --add-modules jdk.jdwp.agent \
+      --add-modules jdk.jfr \
+      --add-modules jdk.jsobject \
+      --add-modules jdk.localedata \
+      --add-modules jdk.management \
+      --add-modules jdk.management.agent \
+      --add-modules jdk.management.jfr \
+      --add-modules jdk.naming.dns \
+      --add-modules jdk.naming.ldap \
+      --add-modules jdk.naming.rmi \
+      --add-modules jdk.net \
+      --add-modules jdk.pack \
+      --add-modules jdk.scripting.nashorn \
+      --add-modules jdk.scripting.nashorn.shell \
+      --add-modules jdk.sctp \
+      --add-modules jdk.security.auth \
+      --add-modules jdk.security.jgss \
+      --add-modules jdk.unsupported \
+      --add-modules jdk.xml.dom \
+      --add-modules jdk.zipfs
 fi
 
 if ! [ -f packr_${PACKR_VERSION}.jar ] ; then
