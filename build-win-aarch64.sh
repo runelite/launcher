41e5b20d410c245461b7bbdca0fc52b8b2027340
Adam <Adam@sigterm.info>
2023-06-25 16:15:43 -0400

    build-win-aarch64: use jlink jre

--- build-win-aarch64.sh
+++ build-win-aarch64.sh
@@ -18,7 +18,24 @@ echo "$WIN_AARCH64_CHKSUM win-aarch64_jre.zip" | sha256sum -c
 if ! [ -d win-aarch64-jdk ] ; then
     unzip win-aarch64_jre.zip
     mkdir win-aarch64-jdk
-    mv jdk-$WIN_AARCH64_VERSION win-aarch64-jdk/jre
+    jlink \
+      --compress 2 \
+      --strip-debug \
+      --no-header-files \
+      --no-man-pages \
+      --output win-aarch64-jdk/jre \
+      --module-path jdk-$WIN_AARCH64_VERSION/jmods \
+      --add-modules java.base,java.compiler,java.datatransfer,java.xml,java.prefs,java.desktop,java.instrument \
+      --add-modules java.logging,java.management,java.security.sasl,java.naming,java.rmi,java.management.rmi \
+      --add-modules java.net.http,java.scripting,java.security.jgss,java.transaction.xa,java.sql,java.sql.rowset \
+      --add-modules java.xml.crypto,java.se,java.smartcardio,jdk.accessibility,jdk.internal.jvmstat,jdk.attach \
+      --add-modules jdk.charsets,jdk.compiler,jdk.crypto.ec,jdk.crypto.cryptoki,jdk.crypto.mscapi,jdk.dynalink \
+      --add-modules jdk.internal.ed,jdk.editpad,jdk.hotspot.agent,jdk.httpserver,jdk.internal.le,jdk.internal.opt \
+      --add-modules jdk.jartool,jdk.javadoc,jdk.jcmd,jdk.management,jdk.management.agent,jdk.jconsole,jdk.jdeps \
+      --add-modules jdk.jdwp.agent,jdk.jdi,jdk.jfr,jdk.jlink,jdk.jshell,jdk.jsobject,jdk.jstatd,jdk.localedata \
+      --add-modules jdk.management.jfr,jdk.naming.dns,jdk.naming.ldap,jdk.naming.rmi,jdk.net,jdk.pack,jdk.rmic \
+      --add-modules jdk.scripting.nashorn,jdk.scripting.nashorn.shell,jdk.sctp,jdk.security.auth,jdk.security.jgss \
+      --add-modules jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,jdk.zipfs
 fi
 
 if ! [ -f packr_${PACKR_VERSION}.jar ] ; then
