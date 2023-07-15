d0cee47f81afde823b1e3ea03aefb3af241f020d
Adam <Adam@sigterm.info>
2023-07-15 10:06:57 -0400

    packr config: set utf8 charset on output stream writer
    Using an OutputStreamWriter also allows gson to write to the stream directly

--- src/main/java/net/runelite/launcher/PackrConfig.java
+++ src/main/java/net/runelite/launcher/PackrConfig.java
@@ -33,8 +33,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.AtomicMoveNotSupportedException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
@@ -98,10 +99,10 @@ class PackrConfig
 
 			try (FileOutputStream fout = new FileOutputStream(tmpFile);
 				FileChannel channel = fout.getChannel();
-				PrintWriter writer = new PrintWriter(fout))
+				OutputStreamWriter writer = new OutputStreamWriter(fout, StandardCharsets.UTF_8))
 			{
 				channel.lock();
-				writer.write(gson.toJson(config));
+				gson.toJson(config, writer);
 				channel.force(true);
 				// FileChannel.close() frees the lock
 			}
