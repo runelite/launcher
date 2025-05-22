f0ad41917eb14084cd8d996d31badee0073bae35
Adam <Adam@sigterm.info>
2025-05-22 16:24:05 -0400

    update jdk to 11.0.27+6 and 17.0.15+6

--- .jdk-versions.sh
+++ .jdk-versions.sh
@@ -1,28 +1,28 @@
 # windows x64
-WIN64_VERSION=11.0.26+4
-WIN64_CHKSUM=48139e97027cc45e03e918e262e2dec8f3bfb113a79cf0ca43c29d4e9dd57ee1
-WIN64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.26%2B4/OpenJDK11U-jre_x64_windows_hotspot_11.0.26_4.zip
+WIN64_VERSION=11.0.27+6
+WIN64_CHKSUM=9ad85fbd8c2983fb6aa9863a6bc7f7e027d3c0f26d459ae8f37351269cb66057
+WIN64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.27%2B6/OpenJDK11U-jre_x64_windows_hotspot_11.0.27_6.zip
 # windows x86
-WIN32_VERSION=11.0.26+4
-WIN32_CHKSUM=34e94694c2600f9e382476ee3064d39e8ba08415640fa8874a7127dc78e7c5b6
-WIN32_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.26%2B4/OpenJDK11U-jre_x86-32_windows_hotspot_11.0.26_4.zip
+WIN32_VERSION=11.0.27+6
+WIN32_CHKSUM=ab5ec2fc99d5d3a13c7d28777a896b33aa8ca94e9cfd38738e30c524066863fb
+WIN32_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.27%2B6/OpenJDK11U-jre_x86-32_windows_hotspot_11.0.27_6.zip
 # windows aarch64
-WIN_AARCH64_VERSION=11.0.26+4
-WIN_AARCH64_CHKSUM=1d10cf760e4e011e830ef18ab28453a742ac84c046e4e77759e81b58716e6a8b
-WIN_AARCH64_LINK=https://aka.ms/download-jdk/microsoft-jdk-11.0.26-windows-aarch64.zip
+WIN_AARCH64_VERSION=11.0.27+6
+WIN_AARCH64_CHKSUM=86cd765690bac2a43a29f3578e933bc918cbac785f3a4be661d901be04e35a17
+WIN_AARCH64_LINK=https://aka.ms/download-jdk/microsoft-jdk-11.0.27-windows-aarch64.zip
 # mac x64
-MAC_AMD64_VERSION=17.0.14+7
-MAC_AMD64_CHKSUM=f2c7454f7aba076cd414887b31da92e4a50fda7a13d97f6e295c911af60de0b6
-MAC_AMD64_LINK=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.14%2B7/OpenJDK17U-jre_x64_mac_hotspot_17.0.14_7.tar.gz
+MAC_AMD64_VERSION=17.0.15+6
+MAC_AMD64_CHKSUM=38f7bb3faaa3aec90290e6dd912a050cc895ee2aa8fb9d8ea6aac86822bb108b
+MAC_AMD64_LINK=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.15%2B6/OpenJDK17U-jre_x64_mac_hotspot_17.0.15_6.tar.gz
 # mac aarch64
-MAC_AARCH64_VERSION=17.0.14+7
-MAC_AARCH64_CHKSUM=9fb89125d5807f42cec588824fde487be42273a89c55ccfc5f44efda64e03e2c
-MAC_AARCH64_LINK=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.14%2B7/OpenJDK17U-jre_aarch64_mac_hotspot_17.0.14_7.tar.gz
+MAC_AARCH64_VERSION=17.0.15+6
+MAC_AARCH64_CHKSUM=2eb9548fbed1031355ca11a35b5a297e9872edd1dafacb40294f0c1a6677bbfb
+MAC_AARCH64_LINK=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.15%2B6/OpenJDK17U-jre_aarch64_mac_hotspot_17.0.15_6.tar.gz
 # linux x64
-LINUX_AMD64_VERSION=11.0.26+4
-LINUX_AMD64_CHKSUM=d26c566a7010d1303d3979b6f076e7911b49419a609c9e4d81f27262bf47f87c
-LINUX_AMD64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.26%2B4/OpenJDK11U-jre_x64_linux_hotspot_11.0.26_4.tar.gz
+LINUX_AMD64_VERSION=11.0.27+6
+LINUX_AMD64_CHKSUM=d854baf8fcf835e28142d1519b88a1367c117e01fa1c18e34f9a1435d02a0437
+LINUX_AMD64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.27%2B6/OpenJDK11U-jre_x64_linux_hotspot_11.0.27_6.tar.gz
 # linux aarch64
-LINUX_AARCH64_VERSION=11.0.26+4
-LINUX_AARCH64_CHKSUM=4ececb5c229763107e9e4acf3b7035db38cf18a98a47176fa5ed1be3f3d15518
-LINUX_AARCH64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.26%2B4/OpenJDK11U-jre_aarch64_linux_hotspot_11.0.26_4.tar.gz
+LINUX_AARCH64_VERSION=11.0.27+6
+LINUX_AARCH64_CHKSUM=e486056040ea7878a6e676bf8fd9112128045b3c1e5230b5dcf73756fc63f7e5
+LINUX_AARCH64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.27%2B6/OpenJDK11U-jre_aarch64_linux_hotspot_11.0.27_6.tar.gz
